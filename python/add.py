42a905a6e7261db379e2a5e72eed5af4dcee20db
Arijit <arijit@easeyourlife.in>
2025-02-14 15:13:25 +0530

    Implementation & update tests (step 7 & 8)

--- python/add.py
+++ python/add.py
@@ -1,11 +1,22 @@
 import re
 
+delimiter_regex = r'\[[^\]]+?\]'
+
 
 def convertToInt(input_str):
     try:
         return int(input_str)
     except:
         return 0
+
+
+def extract_delimiter(match):
+    return match.group()[1:-1]
+
+
+def escape_delimiter(delimiter):
+    escaped_characters = ['\\' + charachter for charachter in delimiter]
+    return ''.join(escaped_characters)
 
 
 def add(numbers: str = None) -> int:
@@ -17,6 +28,13 @@ def add(numbers: str = None) -> int:
 
     if numbers and numbers.startswith("//"):
         delimiter, input_str = numbers[2:].split("\n", 1)
+
+    if isinstance(delimiter, str):
+        if re.search(delimiter_regex, delimiter):
+            matched_delimiters = re.finditer(delimiter_regex, delimiter)
+            delimiter_list = map(extract_delimiter, matched_delimiters)
+            escaped_delimiters = map(escape_delimiter, delimiter_list)
+            delimiter = f"[({'|'.join(escaped_delimiters)})]"
 
     number_list = list(map(convertToInt, re.split(delimiter, input_str)))
 
