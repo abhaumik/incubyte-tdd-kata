792586172a6f5af792176bb6b4fd31d7e786feea
Arijit <arijit@easeyourlife.in>
2025-02-14 14:48:41 +0530

    add base tests (step 1)

--- python/test.py
+++ python/test.py
@@ -5,8 +5,16 @@ from add import add
 
 class TestAddFunction(unittest.TestCase):
 
+    # Test simple inputs
     def test_empty_input(self):
         self.assertEqual(add(), 0)
+
+    def test_one_input(self):
+        self.assertEqual(add("1"), 1)
+
+    def test_two_inputs(self):
+        self.assertEqual(add("1,5"), 6)
+
 
 
 if __name__ == "__main__":
