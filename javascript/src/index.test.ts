1c3e927665a04a603edce83122d2d02ef6a583cc
Arijit <arijit@easeyourlife.in>
2025-02-14 06:05:43 +0530

    Update function signature & add base tests (step 1)

--- javascript/src/index.test.ts
+++ javascript/src/index.test.ts
@@ -1,5 +1,14 @@
-import { add } from './index';
+import { add } from "./index";
 
-test('runs test', () => {
-  expect(add()).toBe(0);
+describe("Test add function for simple inputs ", () => {
+  it("takes empty input", () => {
+    expect(add()).toBe(0);
+  });
+  it("takes one input", () => {
+    expect(add("1")).toBe(1);
+  });
+  it("takes 2 inputs", () => {
+    expect(add("1,5")).toBe(6);
+  });
 });
+
