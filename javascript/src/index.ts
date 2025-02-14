1a000bf72e6b311bc7acf21d48313ca7200f41ca
Arijit <arijit@easeyourlife.in>
2025-02-14 06:31:38 +0530

    Implementation passing test 4

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,7 +1,14 @@
 export function add(numbers?: string): number {
+  let delimiter: string | RegExp = /[,\n]/;
+  let input = numbers;
+  
+  if (numbers?.startsWith("//")) {
+    [delimiter, input] = numbers.substring(2).split(/\n(.*)/);
+  }
+
   return (
-    numbers
-      ?.split(/[,\n]/)
+    input
+      ?.split(delimiter)
       .map((n) => Number(n))
       .reduce((acc, current) => acc + current, 0) ?? 0
   );
