d88799a53c776b9a63ed20ee772125db446d286e
Arijit <arijit@easeyourlife.in>
2025-02-14 15:02:34 +0530

    Implementation passing test 1 & 2

--- python/add.py
+++ python/add.py
@@ -1,2 +1,20 @@
+import re
+
+
+def convertToInt(input_str):
+    try:
+        return int(input_str)
+    except:
+        return 0
+
+
 def add(numbers: str = None) -> int:
-    return 0
+    delimiter = r'[,]'
+    input_str = numbers
+
+    if numbers == None:
+        return 0
+
+    number_list = list(map(convertToInt, re.split(delimiter, input_str)))
+
+    return sum(number_list)
