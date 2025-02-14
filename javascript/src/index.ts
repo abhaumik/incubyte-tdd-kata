b2c22341d6622f35ce6f4769e2ae8259f7b5abee
Arijit <arijit@easeyourlife.in>
2025-02-14 06:46:00 +0530

    Implementation passing test 5 & update test 5

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -1,15 +1,22 @@
 export function add(numbers?: string): number {
   let delimiter: string | RegExp = /[,\n]/;
   let input = numbers;
-  
+
   if (numbers?.startsWith("//")) {
     [delimiter, input] = numbers.substring(2).split(/\n(.*)/);
   }
 
-  return (
-    input
-      ?.split(delimiter)
-      .map((n) => Number(n))
-      .reduce((acc, current) => acc + current, 0) ?? 0
-  );
+  const numberArray = input?.split(delimiter).map((n) => Number(n));
+
+  if (!numberArray) {
+    return 0;
+  }
+
+  for (let n of numberArray) {
+    if (n < 0) {
+      throw `negative numbers not allowed ${n}`;
+    }
+  }
+
+  return numberArray?.reduce((acc, current) => acc + current, 0);
 }
